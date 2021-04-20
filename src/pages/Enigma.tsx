fcfc90f5e4e06170355e7098931defb2cc1426d9
Luminilion <nico.darge@gmail.com>
2021-04-20 16:34:19 +0200

    Added enigmas picking logic, work to pick n enigmas randomly

--- src/pages/Enigma.tsx
+++ src/pages/Enigma.tsx
@@ -4,6 +4,26 @@ import './Enigma.css';
 import enigmas from "../res/enigmas.json";
 
 const Enigma: React.FC = () => {
+
+  // Declare parameters for enigmas picking
+  const nb_enigmas = 5;
+
+  // Pick a list of random enigmas from `enigmas` object
+  let enigmas_list = enigmas.sort(() => 0.5 - Math.random()).slice(0, nb_enigmas)
+
+  // Preparing display of chosen enigmas
+  const display_enigmas = enigmas_list.map(id => (
+    <li>
+      Il vous faut cacher les éléments suivants : <ol>
+        {id.cacher.map(e => (<li>{e}</li>))}
+      </ol>
+      Indications : <ol>
+        {id.indications.map(i => (<li>{i}</li>))}
+      </ol>
+      <br />
+    </li>
+  ));
+
   return (
     <IonPage>
 
@@ -14,18 +34,7 @@ const Enigma: React.FC = () => {
       </IonHeader>
 
       <IonContent fullscreen>
-
-        <ul>{enigmas.map(id => (
-          <li>
-            Il vous faut cacher les éléments suivants : <ol>
-              {id.cacher.map(e => (<li>{e}</li>))}
-            </ol>
-            Indications : <ol>
-              {id.indications.map(i => (<li>{i}</li>))}
-            </ol>
-            <br />
-          </li>
-        ))}</ul>
+        <ul>{display_enigmas}</ul>
 
       </IonContent>
 
