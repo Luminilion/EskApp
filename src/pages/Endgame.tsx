aed5ff75b6280ffdb5d04d531965a479889e7ad2
MarwenMo <83205105+MarwenMo@users.noreply.github.com>
2021-05-09 19:57:19 +0200

    Added final page

--- src/pages/Endgame.tsx
+++ src/pages/Endgame.tsx
@@ -1,19 +1,41 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,IonCardContent, IonButton } from '@ionic/react';
 import './Endgame.css';
+import logo from '../img/logo.png';
 
 const Endgame: React.FC = () => {
   return (
     <IonPage>
 
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Endgame</IonTitle>
+        <img src={logo} height="50" width="150" />
+        <IonToolbar >
+          
+
+          <IonTitle class="ion-text-center">Fin de la partie</IonTitle>
         </IonToolbar>
       </IonHeader>
 
-      <IonContent fullscreen>
-        <p>Fill me in!</p>
+      <IonContent color="secondary">
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <div className="hello" >C'est l'équipe bleue qui a gagné !</div>
+        <div className="hello" > Bravo à tous ! </div>
       </IonContent>
+
+      <IonContent color="secondary">
+
+      <IonCardContent > 
+          <IonButton class="roundBuzzer"size="large" shape="round" color="warning" >Retour au Menu</IonButton>
+      </IonCardContent>
+
+      </IonContent>
+      
+
+
+
+      
 
     </IonPage>
   );
